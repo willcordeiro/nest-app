b93776927d6fdab1413ad573303819d48d82d46f
willcordeiro <willcordeiro800@gmail.com>
2022-10-30 00:47:56 -0300

    create and implemented the config for the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaNestModule } from './prisma-nest/prisma-nest.module';
 import { PrismaService } from './prisma-nest/prisma-nest.service';
+import configuration from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaNestModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaNestModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/config/configuration.ts
@@ -0,0 +1,5 @@
+export default () => ({
+  database: {
+    port: process.env.DATABASE_URL,
+  },
+});
