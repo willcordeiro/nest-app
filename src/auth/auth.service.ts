26ea6e275e03e4e37aafcd65f9cc4623a0c0421b
willcordeiro <willcordeiro800@gmail.com>
2022-10-29 23:06:43 -0300

    implemented the dto logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma-nest/prisma-nest.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
+  signup(dto: AuthDto) {
     return { msg: 'i am signup' };
   }
 
